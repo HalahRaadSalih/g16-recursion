b157dc08ccea152edcea06f9b24ba5b9eec46b03
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-03-04 12:10:41 -0800

    initial commit

---
+++ beerBottles.js
@@ -0,0 +1,25 @@
+function nintyNineBottlesOfBeer(n){
+
+  // when there are no bottles left
+  if (n <= 0){
+     console.log("No more bottles of beer on the wall, no more bottles of beer. \nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+     return;
+  }
+
+  // when there is one bottle left
+  if(n === 1){
+     console.log("1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n");
+     nintyNineBottlesOfBeer(n-1);
+
+  }
+  // every other bottle left
+
+  else{
+    console.log(n + " bottles of beer on the wall, " + n + " bottles of beer.\nTake one down and pass it around, "+(n-1)+ " bottles of beer on the wall.\n");
+    nintyNineBottlesOfBeer(n-1);
+  }
+
+
+}
+
+console.log(nintyNineBottlesOfBeer(10));
